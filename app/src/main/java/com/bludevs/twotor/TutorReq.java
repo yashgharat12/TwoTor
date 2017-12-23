c3112a2433b8f3e118f0f2b2b8fd60765ba83f3a
yashgharat12 <34743750+yashgharat12@users.noreply.github.com>
2017-12-22 22:50:39 -0500

    Date/Time Selection Setup

--- app/src/main/java/com/bludevs/twotor/TutorReq.java
+++ app/src/main/java/com/bludevs/twotor/TutorReq.java
@@ -1,7 +1,11 @@
 package com.bludevs.twotor;
 
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
+import android.icu.util.GregorianCalendar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -9,9 +13,16 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.kunzisoft.switchdatetime.SwitchDateTimeDialogFragment;
+import com.kunzisoft.switchdatetime.SwitchDateTimeDialogFragment.OnButtonWithNeutralClickListener;
+
+import java.util.Date;
+import java.util.Locale;
+
 public class TutorReq extends AppCompatActivity {
     public String final_topic, final_desc, final_subj;
     Spinner subjSpin;
+    private SwitchDateTimeDialogFragment dateTimeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +38,7 @@ public class TutorReq extends AppCompatActivity {
 
         final EditText topic = (EditText) findViewById(R.id.eTopic);
         final EditText desc = (EditText) findViewById(R.id.eExtra);
-        final TextView date = (TextView) findViewById(R.id.Date);
+        final TextView dateText = (TextView) findViewById(R.id.Date);
 
         Button bDate = (Button) findViewById(R.id.bDate);
         Button bOK = (Button) findViewById(R.id.bOK);
@@ -51,13 +62,54 @@ public class TutorReq extends AppCompatActivity {
             }
         });
 
-        bDate.setOnClickListener(new View.OnClickListener() {
+        dateTimeFragment = (SwitchDateTimeDialogFragment) getSupportFragmentManager().findFragmentByTag("TAG_DATETIME_FRAGMENT");
+        if (dateTimeFragment == null) {
+            dateTimeFragment = SwitchDateTimeDialogFragment.newInstance(
+                    getString(R.string.label_datetime_dialog),
+                    getString(android.R.string.ok),
+                    getString(android.R.string.cancel)
+            );
+        }
+
+        final SimpleDateFormat dateFormat = new SimpleDateFormat("d MMM yyyy HH:mm", java.util.Locale.getDefault());
+        dateTimeFragment.set24HoursMode(false);
+        dateTimeFragment.setMinimumDateTime(new GregorianCalendar(2000, Calendar.JANUARY, 1).getTime());
+        dateTimeFragment.setMaximumDateTime(new GregorianCalendar(2050, Calendar.DECEMBER, 31).getTime());
+
+        try {
+            dateTimeFragment.setSimpleDateMonthAndDayFormat(new java.text.SimpleDateFormat("MMMM dd", Locale.getDefault()));
+        } catch (SwitchDateTimeDialogFragment.SimpleDateMonthAndDayFormatException e) {
+            Log.e("UPDATE", e.getMessage());
+        }
+
+        dateTimeFragment.setOnButtonClickListener(new OnButtonWithNeutralClickListener() {
+
             @Override
-            public void onClick(View v) {
+            public void onNeutralButtonClick(Date date) {
+                dateText.setText("");
+            }
+
+            @Override
+            public void onPositiveButtonClick(Date date) {
+                dateText.setText(dateFormat.format(date));
+            }
+
+            @Override
+            public void onNegativeButtonClick(Date date) {
 
             }
         });
 
+        bDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dateTimeFragment.startAtCalendarView();
+                ;
+                Calendar c = Calendar.getInstance();
+                dateTimeFragment.setDefaultDateTime(c.getTime());
+                dateTimeFragment.show(getSupportFragmentManager(), "TAG_DATETIME_FRAGMENT");
+            }
+        });
 
     }
 }
