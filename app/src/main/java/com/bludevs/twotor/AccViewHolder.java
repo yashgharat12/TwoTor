440cce5392c70de8018fb8ad9499c22433627966
syre <yashgharat12@gmail.com>
2018-02-25 21:57:22 -0500

    Node changing and transfer to accepted_tab works

---
+++ app/src/main/java/com/bludevs/twotor/AccViewHolder.java
@@ -0,0 +1,15 @@
+package com.bludevs.twotor;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by syre on 2/25/18.
+ */
+
+public class AccViewHolder extends RecyclerView.ViewHolder {
+
+    public AccViewHolder(View itemView) {
+        super(itemView);
+    }
+}
